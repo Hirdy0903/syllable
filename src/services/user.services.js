aeed53865e81f248e12e7b2a85c056694efe2f01
Hirdyansh <hirdyanshsaxena7@gmail.com>
2026-06-16 14:13:36 +0530

    phase 1 complted Express PostgreSQL Prisma Auth Error Handling

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,18 +1,12 @@
 import {
   findUserByEmail,
-  createUser,
+  
 } from "../repositories/user.repository.js";
+import { findUserById } from "../repositories/user.repository.js";
 
 export const getUserByEmail = async (email) => {
   return findUserByEmail(email);
 };
-
-export const registerUser = async (userData) => {
-  const existingUser = await findUserByEmail(userData.email);
-
-  if (existingUser) {
-    throw new Error("User already exists");
-  }
-
-  return createUser(userData);
+export const getCurrentUser = async (userId) => {
+  return findUserById(userId);
 };
