e4a580a90ad8e58191dfbfeb14f6853f39697546
Hirdyansh <hirdyanshsaxena7@gmail.com>
2026-06-17 11:42:06 +0530

    redis initialised and setup

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -1,15 +1,25 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { generateRoadmap } from "../services/ai.services.js";
 import { createRoadmap } from "../repositories/roadmap.repository.js";
+import redisClient from "../lib/redis.js";
 
 export const generateRoadmapController = asyncHandler(
   async (req, res) => {
     const { goal } = req.body;
 
-    const roadmap = await generateRoadmap(goal);
+    const cacheKey = `roadmap:${goal.toLowerCase()}`;
 
-    console.log("ROADMAP KEYS:", Object.keys(roadmap));
-    console.log("TITLE:", roadmap.title);
+    const cachedRoadmap = await redisClient.get(cacheKey);
+
+    if (cachedRoadmap) {
+      return res.status(200).json({
+        success: true,
+        source: "cache",
+        data: JSON.parse(cachedRoadmap),
+      });
+    }
+
+    const roadmap = await generateRoadmap(goal);
 
     const savedRoadmap = await createRoadmap({
       title: roadmap.title,
@@ -18,8 +28,17 @@ export const generateRoadmapController = asyncHandler(
       userId: req.user.userId,
     });
 
+    await redisClient.set(
+      cacheKey,
+      JSON.stringify(savedRoadmap),
+      {
+        EX: 60 * 60 * 24, // 24 hours
+      }
+    );
+
     res.status(201).json({
       success: true,
+      source: "gemini",
       data: savedRoadmap,
     });
   }
